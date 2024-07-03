0135590b97b46befba57a303109f22b193f573c5
Michael Mammoliti <mammoliti.michael@gmail.com>
2024-07-03 16:42:12 +0100

    new build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,24 +1,25 @@
-const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
-const { DefinePlugin } = require("webpack");
-const webpackTemplate = require("./webpack.template.js");
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const { DefinePlugin, CleanPlugin } = require('webpack');
+const webpackTemplate = require('./webpack.template.js');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
-require("./webpack.utils.js");
+require('./webpack.utils.js');
 
 module.exports = {
   entry: {
-    app: "./src/index.tsx",
+    app: './src/index.tsx',
   },
-  mode: "production",
+  mode: 'production',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: [
           {
-            loader: "ts-loader",
+            loader: 'ts-loader',
             options: {
               transpileOnly: true,
             },
@@ -28,16 +29,16 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.gltf$/i,
+        test: /\.(gltf|patt|dat)$/i,
         use: [
           {
             loader: 'file-loader',
             options: {
               publicPath: './',
-              name: '[name].[ext]'
+              name: '[name].[ext]',
             },
           },
         ],
@@ -45,37 +46,37 @@ module.exports = {
       {
         test: /\.scss$/i,
         use: [
-          "style-loader",
+          'style-loader',
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               esModule: false,
               modules: {
-                mode: "local",
+                mode: 'local',
                 exportGlobals: true,
                 namedExport: true,
-                exportLocalsConvention: "as-is",
+                exportLocalsConvention: 'as-is',
                 exportOnlyLocals: false,
-                localIdentName: "[local]--[hash:base64:5]",
+                localIdentName: '[local]--[hash:base64:5]',
               },
             },
           },
-          "sass-loader",
+          'sass-loader',
         ],
       },
       {
         test: /\.(png|jpe?g|gif|mp3)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
           },
         ],
       },
     ],
   },
-  stats: "errors-only",
+  stats: 'errors-only',
   devServer: {
-    static: "./src",
+    static: './src',
     compress: true,
     port: 3000,
     hot: true,
@@ -88,27 +89,27 @@ module.exports = {
     historyApiFallback: true,
     proxy: [
       {
-        context: ["/api"],
-        target: "http://localhost:3001",
-        pathRewrite: { "^/api": "/v1" },
+        context: ['/api'],
+        target: 'http://localhost:3001',
+        pathRewrite: { '^/api': '/v1' },
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "./",
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: './',
     pathinfo: false,
     clean: true,
-    filename: "[name].bundle.js",
-    chunkFilename: "[name].bundle.js",
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
   },
   plugins: [
     new DefinePlugin({
-      NODE_ENV: JSON.stringify("production"),
+      NODE_ENV: JSON.stringify('production'),
     }),
     new ForkTsCheckerWebpackPlugin(),
     new HtmlWebpackPlugin({
@@ -116,5 +117,8 @@ module.exports = {
       hash: true,
     }),
     new CaseSensitivePathsPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: 'public', to: 'public' }],
+    }),
   ],
 };
