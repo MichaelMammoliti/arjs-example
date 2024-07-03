4d54cb4d2eb43ab7abf42429942ee9a23bf3fef1
Michael Mammoliti <mammoliti.michael@gmail.com>
2024-07-03 16:44:18 +0100

    fixed dist

--- notes.md
+++
@@ -1,3 +0,0 @@
-Converting a model for missing material in gltf imported from sketchfab:
-
-https://stackoverflow.com/questions/75886578/three-gltfloader-unknown-extension-khr-materials-pbrspecularglossiness
--- webpack.config.analyse.js
+++
@@ -1,7 +0,0 @@
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const prodConfig = require('./webpack.config.prod.js');
-
-module.exports = {
-  ...prodConfig,
-  plugins: [...prodConfig.plugins, new BundleAnalyzerPlugin()],
-};
